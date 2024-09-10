d2e1c0a3269db2906af531d8af32384fb742ffb4
deep-collab <deepakshetty2806@gmail.com>
2024-09-10 14:29:44 +0530

    built_Navbar

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { SlideNavTabs } from "@/components/navComponents/navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <SlideNavTabs/>
+      </div>
+    </div>
+  );
+}
